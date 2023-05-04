84e25a204300737e72ed8d11981cc452a6e09863
Anton Mikhaylov <aa.mikhayloff@yandex.ru>
2023-05-04 23:17:37 +0300

    Main_2

--- Two_sum_folder/Two_sum.py
+++ Two_sum_folder/Two_sum.py
@@ -7,9 +7,7 @@ class Solution:
                     return[i, j]
 
         return[]
-
-sum = Solution.twoSum("", [2, 7, 11, 15], 9)
-# a = [2, 7, 11, 15]
-# t = 9
-# sum(a, t)
-print(sum)
+    """
+    Some text
+    
+    """
