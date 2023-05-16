ed82cc471eb2ffef95cc7b27d271d30b44f82f70
Anton Mikhaylov <aa.mikhayloff@yandex.ru>
2023-05-16 23:29:47 +0300

    Changed files

--- Problems/Two_sum.py
+++ Problems/Two_sum.py
@@ -2,5 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         o = {}
         for i in range(len(nums)):
-            if nums[i] in o: return[i, o[nums[i]]]
+            if nums[i] in o:
+                return [i, o[nums[i]]]
             o[target - nums[i]] = i
